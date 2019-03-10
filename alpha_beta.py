9053522ca692aaa1dc66e212f41c9222204f282d
Leonard-Zhao <44124870+Leonard-Zhao@users.noreply.github.com>
2019-03-10 16:49:19 -0400

    Update alpha_beta.py

--- alpha_beta.py
+++ alpha_beta.py
@@ -1,26 +1,32 @@
+import math
+
 # Actual implementation will depend on how tree and node are implemented
 # All node functions are currently placeholders
 # INPUTS:
-#   t - tree
 #   node - current node
 #   alpha - alpha value
 #   beta - beta value
-#   maxlevel - boolean, True is node is a max node, False if node is a min node
-def alpha_beta(t, node, alpha, beta, maxlevel):
+#   isMax - boolean, True if node is a max node, False if node is a min node
+def alpha_beta(node, alpha, beta, isMax):
+    # If node is the root, set initial values for alpha and beta
+    if node.isRoot():
+        alpha = -math.inf
+        beta = math.inf
+
     # If node is a leaf, just return its value
     if node.isLeaf():
         return node.getValue()
 
-    print("Node: {}: [{}, {}]".format(node.getName(), alpha, beta))
+    print("Node {}: [{}, {}]".format(node.getName(), alpha, beta))
 
     # If node is a max node
-    if maxlevel:
+    if isMax:
         for child in node.childNodes:
             # Get max of alpha and the return of the child node
-            alpha = max(alpha, alpha_beta(t, child, alpha, beta, not maxlevel))
-            print("Node: {}: [{}, {}]".format(node.getName(), alpha, beta))
+            alpha = max(alpha, alpha_beta(child, alpha, beta, not maxlevel))
+            print("Node {}: [{}, {}]".format(node.getName(), alpha, beta))
             if alpha >= beta:
-                # Alpha cut
+                # Alpha cut - no point in checking the rest
                 return beta
         # Alpha is the best value
         return alpha
@@ -28,10 +34,10 @@ def alpha_beta(t, node, alpha, beta, maxlevel):
     else:
         for child in node.childNodes:
             # Get min of beta and the return of the child node
-            beta = min(beta, alpha_beta(t, child, alpha, beta, not maxlevel))
-            print("Node: {}: [{}, {}]".format(node.getName(), alpha, beta))
+            beta = min(beta, alpha_beta(child, alpha, beta, not maxlevel))
+            print("Node {}: [{}, {}]".format(node.getName(), alpha, beta))
             if alpha >= beta:
-                # Beta cut
+                # Beta cut - no point in checking the rest
                 return alpha
         # Beta is the best value
         return beta
